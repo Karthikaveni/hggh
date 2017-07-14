b60b504d5c4e6619c60d5de72638ce36171835ee
Karthikaveni <karthikaveni18@gmail.com>
2017-07-14 12:26:39 +0530

    Create Pro57.java

---
+++ Pro57.java
@@ -0,0 +1,36 @@
+# hggh
+  import java.util.Scanner;
+
+public class Pro57{
+
+  public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        System.out.println("Enter the first string");
+        String a=s.nextLine();
+        System.out.println("Enter the second string");
+        String b=s.nextLine();
+        int c=0;
+        System.out.print("Output is");
+        if(a.length()>=2 && b.length()>=2)
+        {
+        for(int i=0;i<=a.length()-2;i++)
+        {
+          String a1=a.substring(i,i+2);
+          if(b.contains(a1))
+          {
+            System.out.println("True");
+            c=1;
+            break;
+          }
+       
+        }}
+        if(c==0)
+        {
+          System.out.println("False");
+        
+        }
+    
+    
+  }
+
+}
